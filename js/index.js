e046e024cc376c0be6331a297d98b13c2f6d381a
A2i <abdulfatah.a.ahmad@gmx.com>
2018-12-04 17:13:10 -0500

    Refactored EventListner Methods

--- js/index.js
+++ js/index.js
@@ -1,11 +1,23 @@
-let anchorColor = document.querySelectorAll('a');
-console.log(anchorColor);
+let oneAnchorColor = document.querySelector('a');
+console.log(oneAnchorColor);
 
-forEach.anchorColor(anchor => {
-anchorColor.addEventListener('click', () => { // Click event
-anchorColor.style.opacity = "0.5";
-anchorColor.style.backgroundColor = "blue";
-})
+
+    oneAnchorColor.addEventListener('click', () => { // Click event
+        oneAnchorColor.style.opacity = "0.5";
+        oneAnchorColor.style.backgroundColor = "blue";
+    });
+
+
+
+let anchorButtons = document.querySelectorAll('a');
+console.log(anchorButtons);
+
+anchorButtons.forEach(anchor => {
+    
+    anchor.addEventListener('click', () => { // Click event
+         anchor.style.opacity = "0.5";
+         anchor.style.backgroundColor = "blue";
+     })
 
 });
 
@@ -13,7 +25,7 @@ let images = document.querySelectorAll('img');
 console.log(image);
 images.forEach(image => {
   image.addEventListener('mouseenter', () => { // 1. mouseenter
-    image.classList.toggle('.img-disappear');
+    image.classList.toggle('glow');
   });
   image.addEventListener('mouseleave', () => { // 2. mouseleave
     image.classList.toggle('glow');
